d53f5420faf087141caa096462f826424f733707
aslanjulya-maker <aslanjulya@gmail.com>
2026-09-14 22:48:29 +0200

    initialized display with text

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-void setup() {
-pinMode(4,OUTPUT);
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() { 
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+display.println("iyi \n geceler");
+  display.display();
+
 }
 
 void loop() {
-  digitalWrite(4,HIGH);
-  delay(500);
-  digitalWrite(4,LOW);
-  delay(500);  
+    
   
 }
