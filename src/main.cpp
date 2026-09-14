518735860f973c695df1d290bd0d20e3084e66c6
aslanjulya-maker <aslanjulya@gmail.com>
2026-09-15 00:14:35 +0200

    broken text output

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,66 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <string.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-void setup() { 
+ struct molecule
+{
+  char molecular_formula[50];
+  char trivialname[100];
+
+};
+
+void print_molecular_formula(struct molecule Molecule);
+void print_trivialname(struct molecule Molecule);
+
+
+
+void setup() {
+
+ struct molecule nitric_acid = {
+  "HNO3", 
+  "Sa"
+ };
+ 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  //print_molecular_formula(nitric_acid);
+  //delay(500);
+  print_trivialname(nitric_acid);
+  
+}
+
+
+void loop() {
+}
+
+
+
+
+void print_molecular_formula(struct molecule Molecule){
+  char Molecular_formula[50];
+  strcpy(Molecular_formula, Molecule.molecular_formula);
+
   display.clearDisplay();
   display.setTextColor(SSD1306_WHITE);
   display.setTextSize(2);
   display.setCursor(0, 0);
-display.println("iyi \n geceler");
+  display.println(Molecular_formula);
   display.display();
-
 }
 
-void loop() {
-    
-  
+void print_trivialname(struct molecule Molecule){
+  char Trivialname[100];
+  strcpy(Trivialname, Molecule.trivialname);
+
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.println(Trivialname);
+  display.display();
 }
